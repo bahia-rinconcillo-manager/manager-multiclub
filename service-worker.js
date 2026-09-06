b34371fa60bc00cb247f8d22606e84757a7e40af
jacinga75 <jacinga75@gmail.com>
2026-09-06 20:19:51 +0200

    Manager Online · Multiclub V1.0.22

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "manager-multiclub-v1-0-21-patrocinio-historial-pagos";
+const CACHE = "manager-multiclub-v1-0-22-informes-completos";
 const CORE_FILES = [
   "./",
   "./index.html",
@@ -14,7 +14,7 @@ const CORE_FILES = [
   "./sports-v2702.css?v=27248",
   "./cards-v27249.css?v=27249",
   "./mobile-v27231.css?v=27248",
-  "./app-multiclub-v1021.js?v=1021",
+  "./app-multiclub-v1022.js?v=1022",
   "./sports-v27238-multiclub.js?v=103",
   "./cards-v27249.js?v=106",
   "./calendar-v27247.js?v=104",
