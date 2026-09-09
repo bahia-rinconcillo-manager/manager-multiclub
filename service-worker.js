531a03d63e90126149d2582f09162614c10160e6
jacinga75 <jacinga75@gmail.com>
2026-09-09 22:14:21 +0200

    V1.0.27C

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "manager-multiclub-v1-0-26c-snapshot-tecnico";
+const CACHE = "manager-multiclub-v1-0-27c-cierre-temporada-visible";
 const CORE_FILES = [
   "./",
   "./index.html",
@@ -14,7 +14,7 @@ const CORE_FILES = [
   "./sports-v2702.css?v=27248",
   "./cards-v27249.css?v=27249",
   "./mobile-v27231.css?v=27248",
-  "./app-multiclub-v1026c.js?v=1026c",
+  "./app-multiclub-v1027c.js?v=1027c",
   "./sports-v27238-multiclub.js?v=103",
   "./cards-v27249.js?v=106",
   "./calendar-v27247.js?v=104",
