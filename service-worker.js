c82bc26ef636d663397006883e17d18011806f9e
jacinga75 <jacinga75@gmail.com>
2026-09-10 20:03:23 +0200

    V1.0.27F

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "manager-multiclub-v1-0-27e-ficha-jugador-historico";
+const CACHE = "manager-multiclub-v1-0-27f-visor-historico-jugador";
 const CORE_FILES = [
   "./",
   "./index.html",
@@ -14,7 +14,7 @@ const CORE_FILES = [
   "./sports-v2702.css?v=27248",
   "./cards-v27249.css?v=27249",
   "./mobile-v27231.css?v=27248",
-  "./app-multiclub-v1027e.js?v=1027e",
+  "./app-multiclub-v1027f.js?v=1027f",
   "./sports-v27238-multiclub.js?v=103",
   "./cards-v27249.js?v=106",
   "./calendar-v27247.js?v=104",
