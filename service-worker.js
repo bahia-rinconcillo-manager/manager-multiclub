d392f4e59f85e64bccbfeddf7e80e41c3c4297ad
jacinga75 <jacinga75@gmail.com>
2026-09-12 08:29:48 +0200

    V1.0.27K

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "manager-multiclub-v1-0-27j-fichas-jugadores-tecnicos";
+const CACHE = "manager-multiclub-v1-0-27k-nuevo-jugador-corregido";
 const CORE_FILES = [
   "./",
   "./index.html",
@@ -14,7 +14,7 @@ const CORE_FILES = [
   "./sports-v2702.css?v=27248",
   "./cards-v27249.css?v=27249",
   "./mobile-v27231.css?v=27248",
-  "./app-multiclub-v1027j.js?v=1027j",
+  "./app-multiclub-v1027k.js?v=1027k",
   "./sports-v27238-multiclub.js?v=103",
   "./cards-v27249.js?v=106",
   "./calendar-v27247.js?v=104",
